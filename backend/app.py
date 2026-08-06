95c27a6ac934ac087df62caa2802e78a2303ddf7
Asli <ddfeeff@yandex.ru>
2026-08-06 14:35:18 +0300

    created environment dev, proxy on backend

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        self.send_header("Content-Type", "text/html")
         self.end_headers()
-        self.wfile.write(b"OK")
+        self.wfile.write(b"<i>Hello from Effective Mobile!</i>")
 
 if __name__ == "__main__":
     server = HTTPServer(("0.0.0.0", 8080), Handler)
