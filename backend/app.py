a08582a84e7dfd82ef86d12885a67ffdcec23208
Asli <ddfeeff@yandex.ru>
2026-08-06 11:57:44 +0300

    created basic structure project

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    server.serve_forever()
